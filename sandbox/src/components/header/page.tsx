0264cc53939c2728e7d9c3e23e8f2187b2ac1a09
dnanseldev <dnansel.dev@gmail.com>
2023-08-28 15:14:45 -0300

    28-08-2023 15:14

---
+++ sandbox/src/components/header/page.tsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+    return (
+        <>
+            <nav >
+                <ul className="flex justify-around">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Gallary</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navigation;
