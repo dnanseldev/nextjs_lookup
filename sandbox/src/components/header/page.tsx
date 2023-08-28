36a25b388443213a0469bc31cf03a7c6dba84d91
dnanseldev <dnansel.dev@gmail.com>
2023-08-28 15:50:09 -0300

    28-08-2023 15:49

--- sandbox/src/components/header/page.tsx
+++ sandbox/src/components/header/page.tsx
@@ -1,13 +1,19 @@
 const Navigation = () => {
     return (
         <>
-            <nav >
-                <ul className="flex justify-around">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Gallary</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
+            <nav className="flex flex-row bg-slate-300">
+                <div className="basis-1/12">
+                    <ul className="flex justify-around">
+                        <li className="py-6 hover:text-gray-100"><a href="">Home</a></li>
+                    </ul>
+                </div>
+                <div className="basis-10/12">
+                    <ul className="flex justify-end">
+                        <li className="py-6 hover:text-gray-100"><a href="">Gallery</a></li>
+                        <li className="ml-40 py-6 hover:text-gray-100"><a href="">About</a></li>
+                        <li className="ml-40 py-6 hover:text-gray-100"><a href="">Contact</a></li>
+                    </ul>
+                </div>
             </nav>
         </>
     )
