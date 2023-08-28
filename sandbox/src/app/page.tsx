8c2b8881b08d18874b1f76706667ee923a734d68
dnanseldev <dnansel.dev@gmail.com>
2023-08-28 15:15:31 -0300

    28-08-2023 15:14

--- sandbox/src/app/page.tsx
+++ sandbox/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Navigation from '@/components/header/page'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <h1>Starting project</h1>
-    </main>
+    <>
+      <Navigation />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className='text-gray-700 text-4xl'>Starting application</h1>
+      </main>
+    </>    
   )
 }
